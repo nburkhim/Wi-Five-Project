518e2fe9b42a073be2f332d40ddc026d3cd80712
Anthony Pang <anthonypang99@gmail.com>
2022-03-24 12:50:02 -0400

    Added Google Maps API

--- wi-five/src/components/Map/Map.js
+++ wi-five/src/components/Map/Map.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React from "react";
+import GoogleMapReact from 'google-map-react';
+import heatMapData from './heatmapdata.json'
 
 
-import './Map.css'
-const Map = () => {
+
+export default function SimpleMap() {
+  const defaultProps = {
+    center: {
+      lat: 35.3071,
+      lng: -80.7352
+    },
+    zoom: 16.8
+  };
+
   return (
-    <div className="map-container"></div>
-  )
-}
+    // Important! Always set the container height explicitly
+    <div style={{ height: '70vh', width: '100%' }}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "AIzaSyCH_LrL0jG49y3bH60j4yeC-UcqbhOhMck" }}
+        defaultCenter={defaultProps.center}
+        defaultZoom={defaultProps.zoom}
+        heatmapLibrary={true}
+        heatmap={heatMapData}
 
-export default Map
+      >
+
+      </GoogleMapReact>
+    </div>
+  );
+}
