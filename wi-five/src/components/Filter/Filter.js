72722b166cb82caa5679c8181adc6cda239bf1cf
Po Nguyen <pnguye55@uncc.edu>
2022-03-25 10:30:44 -0400

    Style the button

--- wi-five/src/components/Filter/Filter.js
+++ wi-five/src/components/Filter/Filter.js
@@ -1,16 +1,28 @@
-import React from 'react'
+import React from "react";
+import Button from "@mui/material/Button";
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
+const theme = createTheme({ 
+    palette: {
+        secondary: {
+            main: "#FFD93D"
+        }
+    }
+})
 const Filter = (props) => {
+  return (
+    <div direction="row" spacing={2}>
+      <Button variant="outlined" color="error" onClick={props.heatMapLow}>
+        Low Traffic
+      </Button>
+      <ThemeProvider theme={theme}><Button color="secondary" onClick={props.heatMapMedium}>
+        Medium Traffic
+      </Button></ThemeProvider>     
+      <Button variant="contained" color="success" onClick={props.heatMapHigh}>
+        High Traffic
+      </Button>
+    </div>
+  );
+};
 
-
-    return (
-        <div>
-            <button onClick={props.heatMapLow}>Low Traffic</button>
-            <button onClick={props.heatMapMedium}>Medium Traffic</button>
-            <button onClick={props.heatMapHigh}>High Traffic</button>
-
-        </div>
-    )
-}
-
-export default Filter
+export default Filter;
