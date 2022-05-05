bad529ea85f759097c769b26c5870e91ba02c089
Po Nguyen <pnguye55@uncc.edu>
2022-05-05 12:18:01 -0400

    prettier

--- src/components/info/Info.js
+++ src/components/info/Info.js
@@ -6,28 +6,36 @@ const Info = () => {
     <div>
       <div className="we-are-different">
         <div className="container"></div>
-        <h2 className="title1 title1--mod" style={{ marginLeft: '2%' }}>Our Mission</h2>
+        <h2 className="title1 title1--mod" style={{ marginLeft: "2%" }}>
+          Our Mission
+        </h2>
         <div className="we-are-different__content">
           <div className="snappy">
             <img src={images.snappy_process} alt="" />
             <h3>Easy Process</h3>
             <p>
-              In order to use the application, all you have to do is select the filters that you want applied on the map then let the magic happen!
+              In order to use the application, all you have to do is select the
+              filters that you want applied on the map then let the magic
+              happen!
             </p>
           </div>
           <div className="affordable">
             <img src={images.affordable_prices} alt="" />
             <h3>Free of Cost</h3>
             <p>
-              We wanted to ensure that the application would be free of cost so that any student or faculty member could easily view wi-fi traffic.
+              We wanted to ensure that the application would be free of cost so
+              that any student or faculty member could easily view wi-fi
+              traffic.
             </p>
           </div>
           <div className="people">
             <img src={images.people_first} alt="" />
             <h3>Our Team</h3>
             <p>
-              Our team consists of the following students from Dr. Rudd's ITSC 4155 class:
-            </p><br></br>
+              Our team consists of the following students from Dr. Rudd's ITSC
+              4155 class:
+            </p>
+            <br></br>
             <ul>
               <li>Anthony Pang</li>
               <li>Po Nguyen</li>
